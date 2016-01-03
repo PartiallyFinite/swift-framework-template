f5e6b2eb5dc3a5e70c228fa3074ba7c978922ee8
Greg Omelaenko <greg@mostlyinfinite.com>
2016-01-03 18:50:34 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Universal Framework
+
+A template for a Swift Framework:
+
+- iOS and OS X targets
+- Unit tests
+- MIT license
+- Version number update script
+- Carthage support (shared schemes)
+
+## Usage instructions
+
+1. Rename the project and main code folder
+2. Update the Info.plist file path in the iOS and Mac target settings
+3. Search and replace 'UniversalFramework' with the framework name
+4. Update README
+
---
+++ Tests/Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UniversalFrameworkTests.swift
+//  UniversalFrameworkTests
+//
+//  Created by Greg Omelaenko on 3/01/2016.
+//  Copyright © 2016 Greg Omelaenko. All rights reserved.
+//
+
+import XCTest
+@testable import UniversalFramework
+
+class UniversalFrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
